8ffce3a1b3e6830ed525eb6f969e2fafc997fdb5
Taylam Moura <tay.moura@hotmail.com>
2025-01-10 19:43:30 -0300

    Alterar retorno de metodos de void para ResponseEntity

--- src/main/java/com/alura/forum_hub/Controller/ForumController.java
+++ src/main/java/com/alura/forum_hub/Controller/ForumController.java
@@ -1,17 +1,17 @@
 package com.alura.forum_hub.Controller;
 
 
-import com.alura.forum_hub.Forum.DadosAtualizadosTopicos;
-import com.alura.forum_hub.Forum.DadosForum;
-import com.alura.forum_hub.Forum.DadosTopicos;
-import com.alura.forum_hub.Forum.Forum;
+import com.alura.forum_hub.Forum.*;
 import com.alura.forum_hub.Repository.ForumRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 
@@ -24,43 +24,54 @@ public class ForumController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosForum  dados){
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosForum dados, UriComponentsBuilder uriBuilder){
         if(repository.existsByTitulo(dados.titulo())){
-            System.out.println("Topico já cadastrado.");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Título já cadastrado.");
         }
         if(repository.existsByMensagem(dados.mensagem())){
-            System.out.println("Mensagem já cadastrada");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Mensagem já cadastrada.");
         }
-        Forum forum = new Forum(dados);
-        Forum forumSalvo = repository.save(forum);
+        var forum = new Forum(dados);
+        repository.save(forum);
+
+        var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(forum.getId()).toUri();
+        return  ResponseEntity.created(uri).body(new DadosDetalhados(forum));
     }
 
     @GetMapping
-    public Page<DadosTopicos> listarTopicos(Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosTopicos::new);
+    public ResponseEntity<Page<DadosTopicos>> listarTopicos(Pageable paginacao) {
+        var page =  repository.findAll(paginacao).map(DadosTopicos::new);
+        return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    public Optional<DadosTopicos> listarTopicoUnico(@PathVariable Long id) {
-        return repository.findById(id).map(DadosTopicos::new);
+    public ResponseEntity<DadosTopicos> listarTopicoUnico(@PathVariable Long id) {
+        Optional<Forum> optionalForum = repository.findById(id);
+        return optionalForum.map(forum -> ResponseEntity.ok(new DadosTopicos((forum)))).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public void atualizar(@PathVariable Long id, @RequestBody @Valid  DadosAtualizadosTopicos dados){
+    public ResponseEntity<DadosDetalhados> atualizar(@PathVariable Long id, @RequestBody @Valid  DadosAtualizadosTopicos dados){
         Optional<Forum> optionalForum = repository.findById(id);
         if (optionalForum.isPresent()){
             var atualizarDados = optionalForum.get();
             atualizarDados.atualizar(dados);
-            }
+            return ResponseEntity.ok(new DadosDetalhados(atualizarDados));
+        } else {
+            return ResponseEntity.notFound().build();
         }
+    }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletar(@PathVariable Long id){
+    public ResponseEntity deletar(@PathVariable Long id){
         Optional<Forum> optionalForum = repository.findById(id);
         if (optionalForum.isPresent()){
             repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else{
+            return ResponseEntity.notFound().build();
         }
     }
 }
