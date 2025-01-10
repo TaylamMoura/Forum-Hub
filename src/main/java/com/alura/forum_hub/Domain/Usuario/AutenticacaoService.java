3318510dc7fd85011c924da3d95ac277aae8d6ce
Taylam Moura <tay.moura@hotmail.com>
2025-01-10 19:42:21 -0300

    Criar classe AutenticacaoService

---
+++ src/main/java/com/alura/forum_hub/Domain/Usuario/AutenticacaoService.java
@@ -0,0 +1,19 @@
+package com.alura.forum_hub.Domain.Usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Service
+public class AutenticacaoService  implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
