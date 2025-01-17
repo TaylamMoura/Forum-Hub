02687205bf17d2c45ef88504b921bb86a7743544
Taylam Moura <tay.moura@hotmail.com>
2025-01-17 15:00:44 -0300

    Arrumar indentação

--- src/main/java/com/alura/forum_hub/Controller/AutenticacaoController.java
+++ src/main/java/com/alura/forum_hub/Controller/AutenticacaoController.java
@@ -1,8 +1,8 @@
 package com.alura.forum_hub.Controller;
 
-import com.alura.forum_hub.Domain.Usuario.DadosAutenticacao;
+import com.alura.forum_hub.DTO.DadosAutenticacao;
 import com.alura.forum_hub.Domain.Usuario.Usuario;
-import com.alura.forum_hub.Infra.Security.DadosTokenJWT;
+import com.alura.forum_hub.DTO.DadosTokenJWT;
 import com.alura.forum_hub.Infra.Security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +28,6 @@ public class AutenticacaoController {
     public ResponseEntity login(@RequestBody @Valid DadosAutenticacao dados){
         var auntenticarToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var autenticacao = manager.authenticate(auntenticarToken);
-
 
         var tokenJWT = tokenService.gerarToken((Usuario) autenticacao.getPrincipal());
         return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
