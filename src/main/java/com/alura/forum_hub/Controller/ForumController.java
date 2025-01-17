81fe03b11d9bf194dabbfa11f3ac674f6a00d275
Taylam Moura <tay.moura@hotmail.com>
2025-01-17 15:06:34 -0300

    Usar TopicosService para chamar metodos

--- src/main/java/com/alura/forum_hub/Controller/ForumController.java
+++ src/main/java/com/alura/forum_hub/Controller/ForumController.java
@@ -1,13 +1,16 @@
 package com.alura.forum_hub.Controller;
 
-
-import com.alura.forum_hub.Forum.*;
+import com.alura.forum_hub.DTO.DadosAtualizadosTopicos;
+import com.alura.forum_hub.DTO.DadosDetalhados;
+import com.alura.forum_hub.DTO.DadosForum;
+import com.alura.forum_hub.DTO.DadosTopicosGeral;
+import com.alura.forum_hub.Domain.Forum.Forum;
 import com.alura.forum_hub.Repository.ForumRepository;
+import com.alura.forum_hub.Service.TopicosService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -22,56 +25,42 @@ public class ForumController {
     @Autowired
     private ForumRepository repository;
 
+    @Autowired
+    private TopicosService service;
+
+
     @PostMapping
     @Transactional
     public ResponseEntity cadastrar(@RequestBody @Valid DadosForum dados, UriComponentsBuilder uriBuilder){
-        if(repository.existsByTitulo(dados.titulo())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Título já cadastrado.");
-        }
-        if(repository.existsByMensagem(dados.mensagem())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Mensagem já cadastrada.");
-        }
-        var forum = new Forum(dados);
-        repository.save(forum);
-
+        var forum = service.cadastrarTopico(dados);
         var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(forum.getId()).toUri();
         return  ResponseEntity.created(uri).body(new DadosDetalhados(forum));
     }
 
     @GetMapping
-    public ResponseEntity<Page<DadosTopicos>> listarTopicos(Pageable paginacao) {
-        var page =  repository.findAll(paginacao).map(DadosTopicos::new);
+    public ResponseEntity<Page<DadosTopicosGeral>> listarTopicos(Pageable paginacao) {
+        var page =  repository.findAll(paginacao).map(DadosTopicosGeral::new);
         return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DadosTopicos> listarTopicoUnico(@PathVariable Long id) {
+    public ResponseEntity<DadosDetalhados> listarTopicoUnico(@PathVariable Long id) {
         Optional<Forum> optionalForum = repository.findById(id);
-        return optionalForum.map(forum -> ResponseEntity.ok(new DadosTopicos((forum)))).orElseGet(() -> ResponseEntity.notFound().build());
+        return optionalForum.map(forum -> ResponseEntity.ok(new DadosDetalhados((forum)))).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<DadosDetalhados> atualizar(@PathVariable Long id, @RequestBody @Valid  DadosAtualizadosTopicos dados){
-        Optional<Forum> optionalForum = repository.findById(id);
-        if (optionalForum.isPresent()){
-            var atualizarDados = optionalForum.get();
-            atualizarDados.atualizar(dados);
-            return ResponseEntity.ok(new DadosDetalhados(atualizarDados));
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<DadosDetalhados> atualizar(@PathVariable Long id, @RequestBody @Valid DadosAtualizadosTopicos dados){
+        var forum = service.atualizarTopico(id, dados);
+        return ResponseEntity.ok(new DadosDetalhados(forum));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity deletar(@PathVariable Long id){
-        Optional<Forum> optionalForum = repository.findById(id);
-        if (optionalForum.isPresent()){
-            repository.deleteById(id);
-            return ResponseEntity.noContent().build();
-        } else{
-            return ResponseEntity.notFound().build();
+        service.deletarTopico(id);
+        return ResponseEntity.noContent().build();
         }
     }
-}
+
