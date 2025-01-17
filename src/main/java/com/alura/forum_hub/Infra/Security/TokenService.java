ec8e4d6663cfe74687276224b3c011b481b15ac1
Taylam Moura <tay.moura@hotmail.com>
2025-01-17 15:16:21 -0300

    Adicionar metodo getSubject e configurar expiração do token de segurança

--- src/main/java/com/alura/forum_hub/Infra/Security/TokenService.java
+++ src/main/java/com/alura/forum_hub/Infra/Security/TokenService.java
@@ -4,13 +4,12 @@ import com.alura.forum_hub.Domain.Usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.security.interfaces.RSAPublicKey;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class TokenService {
@@ -18,21 +17,38 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
+    @Value("${api.security.token.expiration}")
+    private Long expiration;
+
+
     public String gerarToken(Usuario usuario) {
+        Date dataAtual = new Date();
+        Date dataExpiration = new Date(dataAtual.getTime() + expiration);
         try {
             RSAPublicKey rsaPublicKey;
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("Forum Hub")
                     .withSubject(usuario.getLogin())
-                    .withExpiresAt(dataExpiracao())
+                    .withExpiresAt(dataExpiration)
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
     }
 
-    private Instant dataExpiracao() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    public Optional<String> getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return Optional.ofNullable(
+                    JWT.require(algoritmo)
+                            .withIssuer("Forum Hub")
+                            .build()
+                            .verify(tokenJWT)
+                            .getSubject()
+            );
+        } catch (JWTVerificationException exception) {
+            return Optional.empty();
+        }
     }
 }
